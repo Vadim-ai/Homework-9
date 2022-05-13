ef31e1a2fec9e5e342a191b2dffa0533380209d9
SkyPro <vadim.phobia@gmail.com>
2022-05-13 11:55:36 +0300

    Выполнил ДЗ №9 (исправил ошибку со связью книги и автора)

--- src/HomeworkLesson9/Book.java
+++ src/HomeworkLesson9/Book.java
@@ -1,25 +1,51 @@
 package HomeworkLesson9;
 
 public class Book {
-        private String nameOfBook;
-        private int yearOfPublication;
+    private String nameOfBook;
+    private int yearOfPublication;
+    private Author author;
 
-        public Book (String nameOfBook, int yearOfPublication) {
-            this.nameOfBook = nameOfBook;
-            this.yearOfPublication  = yearOfPublication;
-        }
-       public String getNameOfBook(){
-            return this.nameOfBook;
-        }
-
-        public int getYearOfPublication(){
-            return this.yearOfPublication;
-        }
-
-        public void setYearOfPublication(int yearOfPublication) {
-            this.yearOfPublication = yearOfPublication;
-        }
-
+    public Book(String nameOfBook, int yearOfPublication, Author author) {
+        this.nameOfBook = nameOfBook;
+        this.yearOfPublication = yearOfPublication;
+        this.author = author;
     }
+
+    public String getNameOfBook() {
+        return this.nameOfBook;
+    }
+
+    public int getYearOfPublication() {
+        return this.yearOfPublication;
+    }
+
+
+    public void setYearOfPublication(int yearOfPublication) {
+        this.yearOfPublication = yearOfPublication;
+    }
+}
+class Author {
+
+    private String nameAuthor;
+    private String surnameAuthor;
+
+    public Author(String nameAuthor, String surnameAuthor) {
+        this.nameAuthor = nameAuthor;
+        this.surnameAuthor = surnameAuthor;
+    }
+
+    public String getNameAuthor() {
+        return this.nameAuthor;
+    }
+
+    public String getSurnameAuthor() {
+        return this.surnameAuthor;
+    }
+
+}
+
+
+
+
 
 
--- src/HomeworkLesson9/Main.java
+++ src/HomeworkLesson9/Main.java
@@ -4,15 +4,17 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Book harryPotter1 = new Book("Гарри Поттер и Философский Камень", 1995);
-        Book theLordOfRings1 = new Book("Властелин Колец", 1954);
-        Author rouling = new Author("Джоан","Роулинг");
+
+
+        Author rouling = new Author("Майк", "Вазовски");
         Author tolkin = new Author("Джон","Толкин");
 
-        System.out.println("Имя автора : " + tolkin.getNameAuthor() + ". Фамилия автора : " + tolkin.getSurnameAuthor() + ". Книга : " + theLordOfRings1.getNameOfBook()); //тестил
+        Book harryPotter1 = new Book("Гарри Поттер и Философский Камень", 1995,rouling);
+        Book theLordOfRings1 = new Book("Властелин Колец", 1954, tolkin);
+
+
         harryPotter1.setYearOfPublication(1994); // заменил год
         System.out.println("Книга : " + harryPotter1.getNameOfBook() + ". Год издания :" + harryPotter1.getYearOfPublication()); //тестил
-
 
     }
 }
